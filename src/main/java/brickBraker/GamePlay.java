4f992c2e7a74ef18080f8dd27b7ce49af403757b
laura <laurab90>
2022-01-03 17:02:54 -0500

    added the blocks

--- src/main/java/brickBraker/GamePlay.java
+++ src/main/java/brickBraker/GamePlay.java
@@ -2,13 +2,13 @@ package brickBraker;
 
 
 import javax.swing.JPanel;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.Timer;
-import java.awt.Color;
+import java.awt.Graphics2D;
 
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
@@ -27,7 +27,10 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private int ballXdir = -1;
     private int ballYdir = -2;
 
+    private MapGenerator map;
+
     public GamePlay() {
+        map = new MapGenerator(3, 7);
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -54,12 +57,47 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         g.setColor(Color.yellow);
         g.fillOval(ballposX, ballposY, 20, 20);
 
+        // the map
+        map.draw((Graphics2D)g);
+
         g.dispose();
     }
 
         @Override
         public void actionPerformed (ActionEvent e){
             timer.start();
+
+            //how the ball moves
+
+            if(play){
+                //this IF is to make the ball bounce when it touches the base
+                if (new Rectangle(ballposX, ballposY, 20, 20).intersects
+                        (new Rectangle(playerX, 550, 100, 8))){
+                    ballYdir = - ballYdir;
+                }
+
+                //to make the ball touch the blocks
+
+
+
+                //if it's touching the top, left or right
+                ballposX += ballXdir;
+                ballposY += ballYdir;
+                //left
+                if (ballposX < 0){
+                    ballXdir = -ballXdir;
+                }
+                //top
+                if (ballposY < 0){
+                    ballYdir = -ballYdir;
+                }
+                // right
+                if (ballposX > 670){
+                    ballXdir = -ballXdir;
+                }
+            }
+
+            // calling the paint function again
             repaint();
         }
 
@@ -87,9 +125,6 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
                 }
             }
         }
-
-
-
 
 
     public void moveRight() {
